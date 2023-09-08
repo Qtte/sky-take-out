c6240a20d4b51d37565dd278ba2d09c069c33846
tomato <iq8899@qq.com>
2023-09-08 12:22:38 +0800

    新增员工和员工分页查询

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +27,19 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        // Duplicate entry 'zhangsan' for key 'employee.idx_username'
+        // 返回重复信息
+        String message = ex.getMessage();
+        if(message.contains("Duplicate entry")){  //有重复的键值对
+            String split[] = message.split(" ");
+            String username = split[2];  //第三个信息即为用户名
+            String msg = username + MessageConstant.ALREADY_EXIST;
+            return Result.error(msg);
+        }else{
+            return Result.error("未知错误");
+        }
+
+    }
 }
